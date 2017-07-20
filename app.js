5d759ca205bdaf4c5ff63b6d213358bc7a57ced4
b-rohit <rohitbohra9@gmail.com>
2017-07-20 15:10:16 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+      res.sendfile('index.html');
+});
+io.on('connection', function(socket){
+      console.log('A user connected');
+
+      setTimeout(function(){
+          socket.emit('testerEvent', { description: 'A custom event named testerEvent!'});
+                }, 4000);
+
+        //Whenever someone disconnects this piece of code executed
+        socket.on('disconnect', function () {
+                console.log('A user disconnected');
+                  });
+        socket.on('clientEvent', function(data){
+                console.log(data);
+                  });
+});
+
+http.listen(3000, function(){
+      console.log('listening on *:3000');
+});
